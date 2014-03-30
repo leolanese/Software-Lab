39314c51af7665c3c04438a3db651d92e98a1481
sirwilliam <developer@leolanese.com>
2014-03-31 00:33:41 +0100

    layout changes {

--- js/lib/lab.js
+++ js/lib/lab.js
@@ -11,12 +11,12 @@ myApp.controller('MainCtrl', ['$scope', function ($scope) {
 }]);
 myApp.controller('LabelCtrl', ['$scope', function ($scope) {
     $scope.name = "I'm Leo.";
-    $scope.jobTitle = "I'm a Front-end Dev.";
+    $scope.jobTitle = "I'm a Front-end Developer";
     $scope.workLocation = "I'm in London, UK";
 }]);
 myApp.controller('FooterCtrl', ['$scope', function ($scope) {
     $scope.t6 = "Blog";
-    $scope.t7 = "Lab";
+    $scope.t7 = "Twitter";
     $scope.t8 = "e-mail";
 }]);
 
